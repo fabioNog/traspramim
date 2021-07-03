396c4e3c14a5b52faa5252780a28713b05c95191
fabionogueiracarmo <fabio.nogueira@soitic.com>
2021-07-03 00:14:57 -0300

    Create meta and links blade

---
+++ resources/views/layouts/links.blade.php
@@ -0,0 +1,10 @@
+<link rel="shortcut icon" href="style/images/favicon.png">
+  <title>Trás Pra Mim</title>
+  <link rel="stylesheet" type="text/css" href="style/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="style/css/plugins.css">
+  <link rel="stylesheet" type="text/css" href="style/revolution/css/settings.css">
+  <link rel="stylesheet" type="text/css" href="style/revolution/css/layers.css">
+  <link rel="stylesheet" type="text/css" href="style/revolution/css/navigation.css">
+  <link rel="stylesheet" type="text/css" href="style/type/type.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="style/css/color/rose.css">
