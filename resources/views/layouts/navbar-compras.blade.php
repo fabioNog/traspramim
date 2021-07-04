b94bf5edf434cfa3c34680b1345cb1179cecba98
fabionogueiracarmo <fabio.nogueira@soitic.com>
2021-07-04 20:41:36 -0300

    Adjusting link

--- resources/views/layouts/navbar-compras.blade.php
+++ resources/views/layouts/navbar-compras.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar transparent nav-uppercase navbar-expand-lg">
 @guest
       <div class="container flex-row justify-content-center">
-        <div class="navbar-brand"><a href="{{url('/')}}"><img src="#" srcset="style/images/logo/logo.png 1x, style/images/logo/logo.png 2x" alt="" /></a></div>
+        <div class="navbar-brand"><a href="{{url('/')}}"><img src="#" srcset="style/images/logo/logo.png 1x, style/images/logo/logo.png 2x" style="width: 50%;" alt="" /></a></div>
         <div class="navbar-other ml-auto order-lg-3">
           <ul class="navbar-nav flex-row align-items-center" data-sm-skip="true">
             <li class="nav-item">
@@ -13,7 +13,7 @@
         <!-- /.navbar-other -->
         <div class="navbar-collapse offcanvas-nav">
           <div class="offcanvas-header d-lg-none d-xl-none">
-            <a href="{{url('/')}}"><img src="#" srcset="style/images/logo/logo.png 1x, style/images/logo/logo.png 2 x" alt="" /></a>
+            <a href="{{url('/')}}"><img src="#" style="width: 50%;" srcset="style/images/logo/logo.png 1x, style/images/logo/logo.png 2 x" alt="" /></a>
             <button class="plain offcanvas-close offcanvas-nav-close"><i class="jam jam-close"></i></button>
           </div>
           <ul class="navbar-nav mx-auto">
@@ -42,7 +42,7 @@
         <!-- /.navbar-other -->
         <div class="navbar-collapse offcanvas-nav">
           <div class="offcanvas-header d-lg-none d-xl-none">
-            <a href="{{url('/')}}"><img src="#" srcset="style/images/logo/logo.png 1x, style/images/logo/logo.png 2 x" alt="" /></a>
+            <a href="{{url('/')}}" style="width: 50%!important;"><img  src="#" srcset="style/images/logo/logo.png 1x, style/images/logo/logo.png 2 x" alt="" /></a>
             <button class="plain offcanvas-close offcanvas-nav-close"><i class="jam jam-close"></i></button>
           </div>
           <!-- /.navbar-nav -->
