f1ba3c22595585d358dc00fd4f6bf4f174d2e50f
fabionogueiracarmo <fabio.nogueira@soitic.com>
2021-07-04 17:12:19 -0300

    Create componente for comprar

---
+++ resources/views/layouts/comprar/checar_comprar.blade.php
@@ -0,0 +1,26 @@
+<div class="wrapper light-wrapper">
+                  <div class="container inner">
+                    <div class="row">
+                      <div class="col-lg-10 offset-lg-1 col-xl-8 offset-xl-2">
+                        <h2 class="section-title mb-40 text-center">Cheque sua Compra</h2>
+                        <form class="fields-black">
+                          <div class="form-group">
+                            <label for="exampleInputName2">Nome do Produto</label>
+                            <input type="text" class="form-control" id="exampleInputName2" placeholder="Ex: Notebook">
+                          </div>
+                          <!-- /.form-group -->
+                          <div class="form-group">
+                            <label for="exampleInputEmail2">Email</label>
+                            <input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+                          </div>
+                          <!-- /.form-group -->
+                          <a class="btn" data-toggle="tab" href="#tab5-2">Próximo</a>
+                        </form>
+                        <!-- /form -->
+                      </div>
+                      <!-- /column -->
+                    </div>
+                    <!-- /.row -->
+                  </div>
+                  <!-- /.container -->
+                </div>
