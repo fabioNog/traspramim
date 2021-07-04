04a284ab48a2e1b8734a62780cc0a2290b973b8a
fabionogueiracarmo <fabio.nogueira@soitic.com>
2021-07-04 20:40:20 -0300

    Alter button for navigation

--- resources/views/layouts/comprar/info_viajar.blade.php
+++ resources/views/layouts/comprar/info_viajar.blade.php
@@ -1,26 +1,36 @@
 <div class="wrapper light-wrapper">
-                  <div class="container inner">
-                    <div class="row">
-                      <div class="col-lg-10 offset-lg-1 col-xl-8 offset-xl-2">
-                        <h2 class="section-title mb-40 text-center">Detalhes de Entrega</h2>
-                        <form class="fields-black">
-                          <div class="form-group">
-                            <label for="exampleInputName2">Nome do Produto</label>
-                            <input type="text" class="form-control" id="exampleInputName2" placeholder="Ex: Notebook">
-                          </div>
-                          <!-- /.form-group -->
-                          <div class="form-group">
-                            <label for="exampleInputEmail2">Email</label>
-                            <input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
-                          </div>
-                          <!-- /.form-group -->
-                          <a class="btn" data-toggle="tab" href="#tab5-2">Próximo</a>
-                        </form>
-                        <!-- /form -->
-                      </div>
-                      <!-- /column -->
-                    </div>
-                    <!-- /.row -->
-                  </div>
-                  <!-- /.container -->
-                </div>
+  <div class="container inner">
+    <div class="row">
+      <div class="col-lg-10 offset-lg-1 col-xl-8 offset-xl-2">
+        <h2 class="section-title mb-40 text-center">Detalhes de Entrega</h2>
+        <form class="fields-black">
+          <div class="form-group">
+            <label for="exampleInputName2">Nome do Produto</label>
+            <input type="text" class="form-control" id="nome" placeholder="Ex: Notebook">
+          </div>
+          <!-- /.form-group -->
+          <div class="form-group">
+            <label for="exampleInputEmail2">Email</label>
+            <input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+          </div>
+          <!-- /.form-group -->
+          <!-- /.form-group -->
+          <div class="form-group row nav nav-tabs nav-tabs-bg">
+            <a class="btn btn-blue col-md-3 offset-md-1" 
+            style="text-align: center;" data-toggle="tab" href="#tab5-3">
+              Proxímo
+            </a>
+            <a class=" btn btn-blue col-md-3" 
+            style="text-align: center;" data-toggle="tab" href="#tab5-1">
+              Voltar
+            </a>
+          </div>
+        </form>
+        <!-- /form -->
+      </div>
+      <!-- /column -->
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container -->
+</div>
