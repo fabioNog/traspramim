0de65f2bf12d9fb75627a53125b2767e5e50b5fd
fabionogueiracarmo <fabio.nogueira@soitic.com>
2021-07-04 02:22:19 -0300

    Add validations and new colluns for table user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,6 +51,8 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            'phone_id' => ['required', 'string', 'max:20'],
+            'cpf' => ['required', 'string', 'max:20'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -67,6 +69,8 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'phone_id' => $data['phone_id'],
+            'cpf' => $data['cpf'],
             'password' => Hash::make($data['password']),
         ]);
     }
