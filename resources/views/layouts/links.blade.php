7200f30211c16b781e08d26c838de39d39d2858a
fabionogueiracarmo <fabio.nogueira@soitic.com>
2021-07-04 10:39:50 -0300

    Alter Style

--- resources/views/layouts/links.blade.php
+++ resources/views/layouts/links.blade.php
@@ -1,13 +1,13 @@
-<link rel="shortcut icon" href="style/images/favicon.png">
+<link rel="shortcut icon" href="{{asset('style/images/favicon.png')}}">
   <title>Trás Pra Mim</title>
-  <link rel="stylesheet" type="text/css" href="style/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="style/css/plugins.css">
-  <link rel="stylesheet" type="text/css" href="style/revolution/css/settings.css">
-  <link rel="stylesheet" type="text/css" href="style/revolution/css/layers.css">
-  <link rel="stylesheet" type="text/css" href="style/revolution/css/navigation.css">
-  <link rel="stylesheet" type="text/css" href="style/type/type.css">
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <link rel="stylesheet" type="text/css" href="style/css/color/rose.css">
+  <link rel="stylesheet" type="text/css" href="{{asset('style/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{asset('style/css/plugins.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{asset('style/revolution/css/settings.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{asset('style/revolution/css/layers.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{asset('style/revolution/css/navigation.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{asset('style/type/type.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{asset('style.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{asset('style/css/color/rose.css')}}">
 
   <!-- Fonts -->
 <link rel="dns-prefetch" href="//fonts.gstatic.com">
